2eb251ca8d7badd36b5382545fe26c14fcdbd529
aidanmanternach <92654166+aidanmanternach@users.noreply.github.com>
2025-01-14 16:11:26 -0800

    Reset

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',  // Make sure your base path is set correctly for GitHub Pages
-  plugins: [react()],
-});
